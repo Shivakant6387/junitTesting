829fd7193d87dded1f96eaed980577a96761c44c
Shivakant6387 <shivakant5875@gmail.com>
2023-04-04 13:20:58 +0530

    added file

---
+++ src/main/java/com/example/junitTestSpringBoot/JunitTestSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.junitTestSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunitTestSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JunitTestSpringBootApplication.class, args);
+	}
+
+}
